1ecc208799dd5a8158419959814abfc68cad5257
39sho <g.39sho@gmail.com>
2023-09-08 13:43:40 +0900

    aboutを追加

---
+++ src/components/About/About.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta } from '@storybook/react';
+import About from '.';
+
+const meta: Meta<typeof About> = {
+    component: About,
+}
+
+export default meta;
+
+export const Default = {
+};
