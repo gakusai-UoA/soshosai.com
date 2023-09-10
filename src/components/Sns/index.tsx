c3e5c3fc273c0cc7b6ac298a03148962ce0f6e09
39sho <g.39sho@gmail.com>
2023-09-10 22:25:27 +0900

    snsのコンポーネントを追加

---
+++ src/components/Sns/index.tsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.css';
+import follow_us from './images/follow_us.svg';
+import message from './images/message.svg';
+import x from './images/x_icon.svg';
+import insta from './images/insta-icon.svg';
+
+const Sns = () => {
+    return (
+        <div className={styles.sns}>
+            <img src={follow_us.src} className={styles.followUs}></img>
+            <img src={message.src} className={styles.message}></img>
+            <div className={styles.links}>
+                <div className={styles.x}>
+                    <img src={x.src}></img>
+                    <span className={styles.address}>@soshosai</span>
+                </div>
+                <div className={styles.insta}>
+                    <img src={insta.src}></img>
+                    <span className={styles.address}>uoa.gakuensai</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sns;
