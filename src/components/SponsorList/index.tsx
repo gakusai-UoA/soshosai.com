e6872424d9a456e6a6f1b9d810d5d671c3813a8a
39sho <g.39sho@gmail.com>
2023-09-19 16:27:54 +0900

    sponsorページを追加

--- src/components/SponsorList/index.tsx
+++ src/components/SponsorList/index.tsx
@@ -4,7 +4,7 @@ type Props = { children: React.ReactNode };
 
 const SponsorList = ({ children }: Props) => {
     return (
-        <div>
+        <div className={styles.wrapper}>
             <div className={styles.center}>
                 順不同、敬称略
             </div>
