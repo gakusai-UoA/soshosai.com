6e09665748b38dcff02478eb33246a0a93694c90
39sho <g.39sho@gmail.com>
2023-09-21 10:15:36 +0900

    headerとdrawerを含むコンポーネント

---
+++ src/components/HeaderDrawer/HeaderDrawer.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta } from '@storybook/react';
+import HeaderDrawer from '.';
+
+const meta: Meta<typeof HeaderDrawer> = {
+    component: HeaderDrawer,
+}
+
+export default meta;
+
+export const Default = {
+};
