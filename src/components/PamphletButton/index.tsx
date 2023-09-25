d6c3e50a169136806482761c8d011fff63772ee8
39sho <g.39sho@gmail.com>
2023-09-25 15:20:04 +0900

    PamphletButtonの大きさを調整

--- src/components/PamphletButton/index.tsx
+++ src/components/PamphletButton/index.tsx
@@ -1,3 +1,4 @@
+import styles from './styles.module.css';
 import valid_btn from './images/valid.svg';
 import invalid_btn from './images/invalid.svg';
 
@@ -8,7 +9,8 @@ type Props = {
 
 const PamphletButton = ({ invalid, link }: Props) => {
     return (
-        <a href={link}>
+        <a href={link} className={styles.button}>
+            {/* そのうちテキストにする */}
             <img src={invalid ? invalid_btn.src : valid_btn.src}></img>
         </a>
     );
