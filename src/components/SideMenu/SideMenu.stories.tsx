18369143872a3ee0654a73963a02bdb5028e6d71
39sho <g.39sho@gmail.com>
2023-09-27 02:05:16 +0900

    storybookを修正

--- src/components/SideMenu/SideMenu.stories.tsx
+++ src/components/SideMenu/SideMenu.stories.tsx
@@ -12,19 +12,23 @@ export const Default = {
         <>
             <div>
                 <div style={{ height: '70vh', backgroundColor: 'skyblue', }}></div>
-                <SideMenu />
-                <Contents>
-                    <Anchor id='1' name='1' />
-                    <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>1</div>
-                    <Anchor id='2' name='2' />
-                    <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>2</div>
-                    <Anchor id='3' name='3' />
-                    <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>3</div>
-                    <Anchor id='4' name='4' />
-                    <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>4</div>
-                    <Anchor id='5' name='5' />
-                    <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>5</div>
-                </Contents>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr 3fr', }}>
+                    <SideMenu />
+                    <div>
+                        <Contents>
+                            <Anchor id='1' name='1' />
+                            <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>1</div>
+                            <Anchor id='2' name='2' />
+                            <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>2</div>
+                            <Anchor id='3' name='3' />
+                            <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>3</div>
+                            <Anchor id='4' name='4' />
+                            <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>4</div>
+                            <Anchor id='5' name='5' />
+                            <div style={{ height: '70vh', backgroundColor: 'gray', border: 'solid white', }}>5</div>
+                        </Contents>
+                    </div>
+                </div>
             </div>
             <div style={{ height: '70vh', backgroundColor: 'skyblue', }}></div>
         </>
